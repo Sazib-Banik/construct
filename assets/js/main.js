157936b4e5c9744e161a80fb709728184ec5bc66
Sazib Banik <sazib.banik.1@gmail.com>
2023-06-24 00:06:16 +0600

    jquery activation add in index page script tag

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,89 +1,89 @@
-// banner carousel 
-$('.main-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    autoplay:true,
-    autoplayTimeout:2000,
-    autoplayHoverPause:true,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:false
-        },
-        600:{
-            items:1,
-            nav:false
-        },
-        1000:{
-            items:1,
-            nav:true,
-            loop:false
-        }
-    }
-})
-// counter js active
-$('.counter').counterUp({
-    delay: 10,
-    time: 3000
-});
+// // banner carousel 
+// $('.main-carousel').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     autoplay:true,
+//     autoplayTimeout:2000,
+//     autoplayHoverPause:true,
+//     responsiveClass:true,
+//     responsive:{
+//         0:{
+//             items:1,
+//             nav:false
+//         },
+//         600:{
+//             items:1,
+//             nav:false
+//         },
+//         1000:{
+//             items:1,
+//             nav:true,
+//             loop:false
+//         }
+//     }
+// })
+// // counter js active
+// $('.counter').counterUp({
+//     delay: 10,
+//     time: 3000
+// });
 
-// init Isotope
-var $grid = $(".grids").isotope({
-  layoutMode: "fitRows",
-});
+// // init Isotope
+// var $grid = $(".grids").isotope({
+//   layoutMode: "fitRows",
+// });
 
-// filter functions
-var filterFns = {
-  // show if number is greater than 50
-  numberGreaterThan50: function () {
-    var number = $(this).find(".number").text();
-    return parseInt(number, 10) > 50;
-  },
-  // show if name ends with -ium
-  ium: function () {
-    var name = $(this).find(".name").text();
-    return name.match(/ium$/);
-  },
-};
-// bind filter button click
-$("#filters").on("click", "button", function () {
-  var filterValue = $(this).attr("data-filter");
-  // use filterFn if matches value
-  filterValue = filterFns[filterValue] || filterValue;
-  $grid.isotope({ filter: filterValue });
-});
-// change is-checked class on buttons
-$(".button-group").each(function (i, buttonGroup) {
-  var $buttonGroup = $(buttonGroup);
-  $buttonGroup.on("click", "button", function () {
-    $buttonGroup.find(".is-checked").removeClass("is-checked");
-    $(this).addClass("is-checked");
-  });
-});
+// // filter functions
+// var filterFns = {
+//   // show if number is greater than 50
+//   numberGreaterThan50: function () {
+//     var number = $(this).find(".number").text();
+//     return parseInt(number, 10) > 50;
+//   },
+//   // show if name ends with -ium
+//   ium: function () {
+//     var name = $(this).find(".name").text();
+//     return name.match(/ium$/);
+//   },
+// };
+// // bind filter button click
+// $("#filters").on("click", "button", function () {
+//   var filterValue = $(this).attr("data-filter");
+//   // use filterFn if matches value
+//   filterValue = filterFns[filterValue] || filterValue;
+//   $grid.isotope({ filter: filterValue });
+// });
+// // change is-checked class on buttons
+// $(".button-group").each(function (i, buttonGroup) {
+//   var $buttonGroup = $(buttonGroup);
+//   $buttonGroup.on("click", "button", function () {
+//     $buttonGroup.find(".is-checked").removeClass("is-checked");
+//     $(this).addClass("is-checked");
+//   });
+// });
 
-$('.testimonial-carousel').owlCarousel({
-  loop:true,
-  margin:10,
-  nav:false,
-  dots:false,
-  autoplay:true,
-  autoplayTimeout:2000,
-  autoplayHoverPause:true,
-  responsiveClass:true,
-  responsive:{
-      0:{
-          items:1,
-          nav:false
-      },
-      600:{
-          items:1,
-          nav:false
-      },
-      1000:{
-          items:1,
-          nav:false,
-          loop:false
-      }
-  }
-})
+// $('.testimonial-carousel').owlCarousel({
+//   loop:true,
+//   margin:10,
+//   nav:false,
+//   dots:false,
+//   autoplay:true,
+//   autoplayTimeout:2000,
+//   autoplayHoverPause:true,
+//   responsiveClass:true,
+//   responsive:{
+//       0:{
+//           items:1,
+//           nav:false
+//       },
+//       600:{
+//           items:1,
+//           nav:false
+//       },
+//       1000:{
+//           items:1,
+//           nav:false,
+//           loop:false
+//       }
+//   }
+// })
